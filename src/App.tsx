58a9a9a0bfdb8d103b4159237f6779b0aac36d1f
bearlogin <bearlogin@mail.ru>
2022-11-16 16:09:50 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {StaticTreeDataProvider, Tree, UncontrolledTreeEnvironment} from 'react-complex-tree';
+import 'react-complex-tree/lib/style.css'
+import {TreeItem, TreeItemIndex} from "react-complex-tree/lib/esm/types";
 
-function App() {
+const Sandbox = () => {
+  const tree1:Record<TreeItemIndex, TreeItem> = {
+    left_root: {
+      index: "left_root",
+      canMove: false,
+      children: ["left_child1", "left_child2"],
+      data: "Left Root item",
+      canRename: true,
+      hasChildren: true,
+    },
+    left_child1: {
+      index: "left_child1",
+      canMove: false,
+      children: ["left_child3"],
+      data: "Left Child item 1",
+      canRename: true,
+      hasChildren: true,
+    },
+    left_child2: {
+      index: "left_child2",
+      canMove: false,
+      children: [],
+      data: "Left Child item 2",
+      canRename: true,
+      hasChildren: false,
+    },
+    left_child3: {
+      index: "left_child3",
+      canMove: false,
+      children: [],
+      data: "Left Child item 3",
+      canRename: true,
+      hasChildren: false,
+    },
+  };
+  const tree2: Record<TreeItemIndex, TreeItem> = {
+    right_root: {
+      index: "right_root",
+      canMove: false,
+
+      children: ["right_child1", "right_child2"],
+      data: "Right Root item",
+      canRename: true,
+      hasChildren: true,
+    },
+    right_child1: {
+      index: "right_child1",
+      canMove: true,
+
+      children: [],
+      data: "Right Child item 1",
+      canRename: true,
+      hasChildren: false,
+    },
+    right_child2: {
+      index: "right_child2",
+      canMove: true,
+      children: [],
+      data: "Right Child item 2",
+      canRename: true,
+      hasChildren: false,
+    },
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="flex">
+        <UncontrolledTreeEnvironment
+            canDragAndDrop={true}
+            canDropOnItemWithChildren={true}
+            canDropOnItemWithoutChildren={true}
+            canReorderItems={true}
+            dataProvider={
+              new StaticTreeDataProvider({ ...tree1, ...tree2 }, (item, data) => ({
+                ...item,
+                data,
+              }))
+            }
+            getItemTitle={item => item.data}
+            onDrop={(items, target) => {
 
-export default App;
+            }}
+            viewState={{}}>
+          <div>
+            <Tree
+                treeId="tree-1"
+                rootItem="left_root"
+                treeLabel="Tree 1"
+            />
+          </div>
+          <div>
+            <Tree treeId="tree-2" rootItem="right_root" treeLabel="Tree 2" />
+          </div>
+        </UncontrolledTreeEnvironment>
+      </div>
+  );
+};
+
+export default Sandbox;
